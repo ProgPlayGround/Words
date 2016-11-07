7730f7b89d2f4aac852889c0428fd5b56c407a1b
Andrew Pryshchepa <vinglfm@gmail.com>
2016-11-08 00:45:58 +0200

    Added js, css, html gulp build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,14 @@ var cssnano = require('gulp-cssnano');
 var argv = require('yargs').argv;
 var autoprefixer = require('gulp-autoprefixer');
 var less = require('gulp-less');
-
+var browserSync = require('browser-sync');
+var htmlmin = require('gulp-htmlmin');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var destDir = 'bin';
 
 gulp.task('bower', function () {
   return bower('libs');
-});
-
-gulp.task('copy-static', function() {
-  return gulp.src(['app/images/**/*.{png,jpg,svg}','app/**/*.html', 'app/**/*.js', '!app/**/*_test.js'])
-      .pipe(gulp.dest(destDir));
 });
 
 gulp.task('css', function() {
@@ -27,4 +25,34 @@ gulp.task('css', function() {
         .pipe(gulpif(argv.prod, cssnano()))
         .pipe(gulpif(!argv.prod, sourcemaps.write()))
         .pipe(gulp.dest(destDir));
+});
+
+gulp.task('css:watch', ['css'], browserSync.reload);
+
+gulp.task('html', function() {
+  return gulp.src(['app/**/*.html', ])
+        .pipe(gulpif(argv.prod, htmlmin({collapseWhitespace: true})))
+        .pipe(gulp.dest(destDir));
+});
+
+gulp.task('html:watch', ['html'], browserSync.reload);
+
+gulp.task('js', function() {
+  return gulp.src(['app/**/*.js', '!app/**/*_test.js'])
+      .pipe(concat('bundle.js'))//check if it works
+      .pipe(gulpif(!argv.prod, sourcemaps.init()))
+      .pipe(gulpif(argv.prod, uglify('bundle.js')))
+      .pipe(gulpif(!argv.prod, sourcemaps.write()))
+      .pipe(gulp.dest(destDir));
+});
+
+gulp.task('watch', function() {
+  browserSync({
+    server: {
+      baseDir: 'bin/'
+    }
+  });
+
+  gulp.watch('app/**/*.html', ['html:watch']);
+  gulp.watch('app/**/*.@(css|less|scss|sss)', ['css:watch']);
 });
