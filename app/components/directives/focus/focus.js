5d78ae1b8824e8dec1b3ba9cacc9cd03742df598
Andrew Pryshchepa <vinglfm@gmail.com>
2016-11-15 09:36:39 +0200

    Added focus directive

---
+++ app/components/directives/focus/focus.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('words')
+    .directive('focus', function() {
+      return {
+        restrict: 'E',
+        controller: function($scope) {
+          $scope.elements = [];
+
+          this.register = function(element) {
+            $scope.elements.push(element);
+          }
+
+          this.next = function(index) {
+            if(index + 1 < $scope.elements.length) {
+              $scope.elements[index].blur();
+              $scope.elements[index + 1].focus();
+            }
+          }
+
+          this.previous = function(index) {
+            if(index - 1 >= 0) {
+              $scope.elements[index].blur();
+              $scope.elements[index - 1].focus();
+            }
+          }
+        }
+      };
+    });
--- app/components/directives/letter/letter.js
+++ app/components/directives/letter/letter.js
@@ -3,23 +3,30 @@
 angular.module('words')
       .directive('letter', function() {
         return {
+          require:'^focus',
           restrict: 'A',
           scope: {
             char: '=',
+            index: '=',
             model: '=ngModel'
           },
-          link: function(scope, element, attrs) {
+          link: function(scope, element, attrs, focusCtrl) {
+            focusCtrl.register(element[0]);
             scope.$watch("model", function (value) {
-              if(!value) {
-                element.removeClass('wrong_answer');
-                element.removeClass('right_answer');
-              } else {
-                if(scope.char == value) {
+              if(value !== undefined) {
+                if(value == '') {
                   element.removeClass('wrong_answer');
-                  element.addClass('right_answer');
-                } else {
                   element.removeClass('right_answer');
-                  element.addClass('wrong_answer')
+                  focusCtrl.previous(scope.index);
+                } else {
+                  if(scope.char == value) {
+                    element.removeClass('wrong_answer');
+                    element.addClass('right_answer');
+                  } else {
+                    element.removeClass('right_answer');
+                    element.addClass('wrong_answer');
+                  }
+                  focusCtrl.next(scope.index);
                 }
               }
             });
