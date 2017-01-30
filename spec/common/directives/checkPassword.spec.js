c809115b63e7071758f94fb3fc7021c2747490d6
Andrew Pryshchepa <vinglfm@gmail.com>
2017-01-31 00:17:01 +0200

    Added unit tests for validation directives

---
+++ spec/common/directives/checkPassword.spec.js
@@ -0,0 +1,26 @@
+'use strict';
+
+describe('check password directive', function() {
+  var scope, element, model;
+
+  beforeEach(module('words'));
+
+  beforeEach(inject(['$rootScope', '$compile', function($rootScope, $compile){
+    scope = $rootScope.$new();
+    scope.model = '';
+    element = $compile('<input check-password ng-model="model"/>')(scope);
+    scope.$apply();
+  }]));
+
+  it('validate ASCII input', function() {
+    element.val('abcdefghijklmnop').triggerHandler('input');
+    scope.$apply();
+    expect(scope.model).toEqual('abcdefghijklmnop');
+  });
+
+  it('fail to validate unicode input', function() {
+    element.val('абвгдежзклмнопрст').triggerHandler('input');
+    scope.$apply();
+    expect(scope.model).not.toBeDefined();
+  });
+});
---
+++ spec/common/directives/errorMessage.spec.js
@@ -0,0 +1,34 @@
+'use strict';
+
+describe('error message directive', function() {
+  var scope, form, model;
+
+  beforeEach(module('words'));
+
+  beforeEach(inject(['$rootScope', '$compile', function($rootScope, $compile){
+    scope = $rootScope.$new();
+    scope.model = '';
+    scope.message = '';
+    $compile('<form name="form"><input name="input" error-message message="message" ng-model="model"/></form>')(scope);
+    form = scope.form;
+    scope.$apply();
+  }]));
+
+  it('do nothing for empty message', function() {
+    expect(form.input.$error).toEqual({});
+  });
+
+  it('adds error to ng-model when message is not empty', function() {
+    scope.message = 'Error message';
+    scope.$apply();
+    expect(form.input.$error).toEqual({error: true});
+  });
+
+  it('remove error from ng-model on input', function() {
+    scope.message = 'Error message';
+    scope.$apply();
+    scope.model = '5';
+    scope.$apply();
+    expect(form.input.$error).toEqual({});
+  });
+});
