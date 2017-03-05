01f83f097eb59064a3c67acec57ca82f210c5b0e
Andrew Pryshchepa <vinglfm@gmail.com>
2017-03-05 19:31:59 +0200

    Added basic sprint quiz

--- app/sprint/sprintCtrl.js
+++ app/sprint/sprintCtrl.js
@@ -1,9 +1,40 @@
 (function() {
-  angular.module('words').controller('SprintCtrl', ['sprintManager', function(sprintManager) {
+  angular.module('words').controller('SprintCtrl', ['sprintManager', 'scoreManager', 'quizModalManager', function(sprintManager, scoreManager, quizModalManager) {
     var vm = this;
     vm.loadingText = 'Loading...';
-    vm.score = function() {
-      return 50;
+
+    vm.word = sprintManager.word;
+    vm.answer = sprintManager.answer;
+    vm.score = scoreManager.get;
+    vm.isLoaded = sprintManager.isLoaded;
+
+    vm.loadQuestion = function() {
+      quizManager.load();
+      onLoad();
+    };
+
+    vm.onAnswer = function(answer) {
+      if(sprintManager.isCorrect(answer)) {
+        scoreManager.onAnswer('CORRECT');
+      }
+      vm.navigate();
+    };
+
+    vm.navigate = function() {
+      if(sprintManager.next()) {
+        vm.nav = true;
+      } else {
+        quizModalManager.finishModal('main');
+      }
+    };
+
+    vm.onTimeFinished = function() {
+      quizModalManager.finishModal('main');
+    };
+
+    function onLoad() {
+      vm.nav = false;
     }
+
   }]);
 }());
--- app/sprint/sprintManager.js
+++ app/sprint/sprintManager.js
@@ -1,7 +1,24 @@
 (function() {
   angular.module('words').factory('sprintManager', ['wordManager', function(wordManager) {
     return {
+      word: function() {
+        return 'sprint';
+      },
+      answer: function() {
+        return 'спринт';
+      },
+      isCorrect: function(answer) {
+        return answer === 'YES';
+      },
+      load: function() {
 
+      },
+      next: function() {
+        return false;
+      },
+      isLoaded: function() {
+        return true;
+      }
     };
   }]);
 }());
