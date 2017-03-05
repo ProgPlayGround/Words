4712241a2817f27cb460b17910c740d8067577eb
Andrew Pryshchepa <vinglfm@gmail.com>
2017-03-05 19:19:00 +0200

    Added main layout for countdown dirrective

---
+++ app/common/directives/countdown.js
@@ -0,0 +1,22 @@
+(function(){
+  angular.module('words').directive('countdown', ['$interval', function($interval) {
+    return {
+      restrict: 'E',
+      templateUrl: 'common/countdown.html',
+      scope: {
+        callback: '&'
+      },
+      link: function(scope, element, attrs) {
+        scope.count = parseInt(attrs.count) || 30;
+        var interval = $interval(function() {
+          if(scope.count === 0) {
+            $interval.cancel(interval);
+            scope.callback();
+          } else {
+            --scope.count;
+          }
+        }, 1000);
+      }
+    };
+  }]);
+}());
