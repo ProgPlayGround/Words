302b8f1acadc2a7d350b98271812f0051ce941c2
Andrew Pryshchepa <vinglfm@gmail.com>
2017-03-21 23:10:47 +0200

    Added basic layout for dictionary page

--- app/dictionary/dictionaryCtrl.js
+++ app/dictionary/dictionaryCtrl.js
@@ -2,6 +2,13 @@
   'use strict';
 
   angular.module('words').controller('DictionaryCtrl', ['dictionaryManager', function(dictionaryManager) {
-    
+    var vm = this;
+
+    vm.words = dictionaryManager.getWords();
+
+    vm.sound = function(url) {
+      console.log(url);
+    };
+
   }]);
 }());
--- app/dictionary/dictionaryManager.js
+++ app/dictionary/dictionaryManager.js
@@ -1,5 +1,65 @@
 (function(){
-  angular.module('words').factory('dictionaryManager', ['', function() {
+  angular.module('words').factory('dictionaryManager', [function() {
 
+    return {
+      getWords: function() {
+        return [
+          {
+            word: 'word',
+            translation: 'слово',
+            audioUrl: '',
+            imageUrl: ''
+          },
+          {
+            word: 'hello',
+            translation: 'привіт',
+            audioUrl: '',
+            imageUrl: ''
+          },
+          {
+            word: 'blossom',
+            translation: 'розцвітати',
+            audioUrl: '',
+            imageUrl: ''
+          },
+          {
+            word: 'correspond',
+            translation: 'відповитати',
+            audioUrl: '',
+            imageUrl: ''
+          },
+          {
+            word: 'severity',
+            translation: 'труднощі',
+            audioUrl: '',
+            imageUrl: ''
+          },
+          {
+            word: 'stage',
+            translation: 'естрада',
+            audioUrl: '',
+            imageUrl: ''
+          },
+          {
+            word: 'carry-on',
+            translation: 'ручний багаж',
+            audioUrl: '',
+            imageUrl: ''
+          },
+          {
+            word: 'embassy',
+            translation: 'посольство',
+            audioUrl: '',
+            imageUrl: ''
+          },
+          {
+            word: 'haggle',
+            translation: 'торговатись',
+            audioUrl: '',
+            imageUrl: ''
+          }
+        ];
+      }
+    };
   }]);
 }());
