6e1ae23a51eda925ea7c4b4196fefc3688900c60
Andrew Pryshchepa <vinglfm@gmail.com>
2017-07-09 21:11:41 +0300

    Fixed submit on error message when different input has changed

--- app/auth/loginCtrl.js
+++ app/auth/loginCtrl.js
@@ -1,8 +1,8 @@
 (function() {
   'use strict';
 
-  angular.module('words').controller('LoginCtrl', ['$state', 'authService', 'fbAuthService', 'vkAuthService', '$log',
-  function($state, authService, fbAuthService, vkAuthService, $log) {
+  angular.module('words').controller('LoginCtrl', ['$state', 'authService', 'fbAuthService', 'vkAuthService',
+  function($state, authService, fbAuthService, vkAuthService) {
     var vm = this;
     vm.email = '';
     vm.password = '';
@@ -13,13 +13,14 @@
       if(vm.email && vm.password) {
         vm.errorClick = false;
         authService.login(vm.email, vm.password, function(response) {
-          $log.log(response);
           if(response.success) {
             $state.go('main');
           } else if(response.data.errorCode == 2) {
-            vm.error.login = response.data.message;
+            vm.error.type = 'email';
+            vm.error.message = response.data.message;
           } else if(response.data.errorCode == 3) {
-            vm.error.password = response.data.message;
+            vm.error.type = 'password';
+            vm.error.message = response.data.message;
           }
         });
       } else {
