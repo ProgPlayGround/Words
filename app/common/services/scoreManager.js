98aec912bf34ac55c4f2c9136bfa9213ca6eb2b0
Andrew Pryshchepa <vinglfm@gmail.com>
2017-09-10 00:23:23 +0300

    Added view rank logic for displaying current user rank ingo

--- app/common/services/scoreManager.js
+++ app/common/services/scoreManager.js
@@ -5,12 +5,11 @@
     var score = wordEndpoint.load(rankingUrl, false);
     return {
       get: function() {
-        return score.rank;
+        return score;
       },
       onAnswer: function(points) {
         if(points > 0) {
-          wordEndpoint.patch(rankingUrl, {'points': points});
-          score.rank += points;
+          score = wordEndpoint.patch(rankingUrl, {'points': points});
         }
       }
     };
