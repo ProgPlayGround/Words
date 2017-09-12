6a3ad0cac459aa51965bb27ccacfc8186a928f86
Andrew Pryshchepa <vinglfm@gmail.com>
2017-09-12 23:16:59 +0300

    Using object id for fb authentication

--- app/common/services/fbAuthService.js
+++ app/common/services/fbAuthService.js
@@ -5,6 +5,12 @@
   .factory('fbAuthService', ['$window', '$cookies', 'config', 'userService', 'fbAppId', 'wordEndpoint',
    function($window, $cookies, config, userService, fbAppId, wordEndpoint) {
 
+     var template = "0".repeat(24);
+
+     function prepareUserId(id) {
+       return (template + id).slice(-24);
+     }
+
     var clearUserData = function() {
       userService.clear();
       $cookies.remove('auth-type');
@@ -14,11 +20,12 @@
     function onConnection(res, callback) {
       if(res.status === 'connected') {
         var accessToken = res.authResponse.accessToken;
-        userService.set('fb' + res.authResponse.userID);
         $cookies.put('auth-type', 'fb');
         $cookies.put('token', accessToken);
-        wordEndpoint.post(config.apiUrl + '/authenticate/fb/registration', {'userId': userService.get()});
-        callback();
+        wordEndpoint.post(config.apiUrl + '/authenticate/fb/registration', {'userId': prepareUserId('fb' + res.authResponse.userID)}).$promise.then(function(data) {
+          userService.set(data.userId);
+          callback();
+        });
       } else {
         clearUserData();
       }
