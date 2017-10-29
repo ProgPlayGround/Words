8ff6963417a91c1c86453c79b9680fc9003f6c15
Andrew Pryshchepa <vinglfm@gmail.com>
2017-10-29 08:46:30 +0200

    Navigating to login page for 401 responses

--- app/common/forbiddenInterceptor.js
+++ app/common/forbiddenInterceptor.js
@@ -3,7 +3,7 @@
   angular.module('words').factory('forbiddenInterceptor', ['$injector', '$q', function($injector, $q) {
     return {
       responseError: function(error) {
-        if(error.status == 403) {
+        if(error.status == 401 || error.status == 403) {
           $injector.get('$state').go('auth.login');
         } else {
           return $q.reject(error);
